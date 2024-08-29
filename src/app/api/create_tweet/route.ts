a836e10ff72da9535cf6838c54d67117c25c3f72
soonhee0 <kyoujunki0@gmail.com>
2024-08-29 12:37:55 +0900

    delete dublicated directories

--- src/app/api/create_tweet/route.ts
+++
@@ -1,8 +0,0 @@
-// モックAPI
-// フロントエンドからのPOSTリクエストをモックして、レスポンスをシミュレーションすることができる
-import { NextRequest, NextResponse } from "next/server";
-
-export function POST(request: NextRequest) {
-  const body = request.body;
-  return NextResponse.json({ isSuceess: true });
-}
