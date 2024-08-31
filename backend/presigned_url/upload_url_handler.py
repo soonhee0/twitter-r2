ceffcb2cda8ad518c684ba491694f3c183a57476
soonhee0 <kyoujunki0@gmail.com>
2024-08-31 16:39:29 +0900

    add fetch tweet api

---
+++ backend/presigned_url/upload_url_handler.py
@@ -0,0 +1,28 @@
+import os
+
+from utils.s3_client import get_s3_client
+
+URL_EXPIRE_TIME = 1800
+
+
+def handler(payload: dict):
+    """
+    署名付きURLの発行ラムダ.
+    /api/upload_url
+    """
+    # S3へ保存するパスを指定してpresigned_urlを取得する
+    generated_presigned_url = get_s3_client().generate_presigned_url(
+        ClientMethod="put_object", # put_object用のURL取得
+        Params={
+            "Bucket": os.getenv("S3_BUCKET_NAME"),
+            # アップロード先のパス
+            "Key": "images/" + payload["userId"] + "/" + payload["tweetId"] + ".png",
+        },
+        ExpiresIn=URL_EXPIRE_TIME,
+    )
+
+    result = {
+        "presignedUrl": generated_presigned_url,
+    }
+
+    return result
